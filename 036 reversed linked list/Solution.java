2bbb53c986bef259fcc67efc2f8ae4ce5625bcb9
Lorenzo De Luca <lorenzo@deluca.pro>
2025-07-04 18:47:37 +0200

    reverse linked list

---
+++ 036 reversed linked list/Solution.java
@@ -0,0 +1,29 @@
+iterative:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;        
+    }
+}
+
+RECURSIVE:
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;        
+    }
+}
