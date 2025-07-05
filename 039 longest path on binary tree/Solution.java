18de8daecf068a2c163b957757706d797d9e9928
Lorenzo De Luca <lorenzo@deluca.pro>
2025-07-05 18:07:49 +0200

    longest path on binary tree

---
+++ 039 longest path on binary tree/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    int res = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;        
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+
+        res = Math.max(res, l + r);
+
+        return 1 + Math.max(l, r);
+    }    
+}
