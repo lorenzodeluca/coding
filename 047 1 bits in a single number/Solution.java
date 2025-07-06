ceeba3820059a00c71ef6cec496f9989b6550314
Lorenzo De Luca <lorenzo@deluca.pro>
2025-07-06 21:55:30 +0200

    1 bits in a single number

---
+++ 047 1 bits in a single number/Solution.java
@@ -0,0 +1,16 @@
+//avg time O(1), space O(1)
+class Solution {
+    public int hammingWeight(int n) {
+        int c=0;
+        while(n>0)
+        {
+            int r=n%2;
+            if(r==1)
+            {
+                c++;
+            }
+            n=n/2;
+        }
+        return c;
+    }
+}
