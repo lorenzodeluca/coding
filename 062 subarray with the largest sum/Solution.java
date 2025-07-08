652c876ce328d5e89b54d96eec725c5d061eb018
Lorenzo De Luca <lorenzo@deluca.pro>
2025-07-08 23:02:30 +0200

    max subarray sum

---
+++ 062 subarray with the largest sum/Solution.java
@@ -0,0 +1,39 @@
+//time O(2^n), space O(target/smallest candidate)
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
+}
+
+/*clean code
+ * class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            currentSum = max(nums[i], currentSum + nums[i]);
+            maxSum = max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+};
+ */
